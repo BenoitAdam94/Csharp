e67fa1259cdffaec50321e8ee7835a311c99b448
adam050986@yahoo.fr <W@c00l00>
2020-07-05 12:58:52 +0200

    init2

---
+++ 01_Helloworld/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Cdieze
+{
+    public class MyClass
+    {
+        public static void AfficherBonjour()
+        {
+            Console.WriteLine("Bonjour");
+        }
+    }
+}
---
+++ 01_Helloworld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cdieze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.AfficherBonjour();
+            // Console.WriteLine("Hello ! Quel est ton prénom ?");
+            /* string pseudo = Console.ReadLine();
+            Console.WriteLine("Bienvenue {0}", pseudo); */
+        }
+    }
+}
